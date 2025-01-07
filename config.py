3b233a4ca373420daa6b220523770da912de2110
Luca Milanesi <milanesiluca2002@gmail.com>
2025-01-07 21:21:42 +0100

    final little improvement on plotting times result

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 CUSTOMERS_NUM = 5000
-"""Represents the number of customer to generate."""
 
 TERMINALS_NUM = 3000
-"""Represents the number of terminals to generate."""
 
 START_DATE = '2024-01-01'
-"""Represents the starting date of the transactions."""
 
 R = 5
-"""Represents the radius within which customers must be located in order to be associated with a terminal."""
+
+NB_DAYS = 500
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 OUTPUT_DIR = os.path.join(PROJECT_ROOT, ".output")
 
 DATASET_OUTPUT_DIR = os.path.join(OUTPUT_DIR, "dataset")
+
+ANALYSIS_OUTPUT_DIR = os.path.join(OUTPUT_DIR, "analysis")
--- test.py
+++ test.py
@@ -4,29 +4,29 @@ from script.generator import Generator
 from script.database import DatabaseInstance
 from script.loader import Loader
 from script.operation import Operations
-from config import OUTPUT_DIR, DATASET_OUTPUT_DIR
+from config import OUTPUT_DIR, DATASET_OUTPUT_DIR, ANALYSIS_OUTPUT_DIR
 from common.logger import logger
-from common.utils import clear_dir_path
+from common.utils import clear_dir_path, create_plot
 
 
 def main():
-    load_dotenv()
-    # clear_dir_path(OUTPUT_DIR)
-
-    db = DatabaseInstance(
-        uri=os.getenv("DATABASE_URI"),
-        user=os.getenv("DATABASE_USER"),
-        password=os.getenv("DATABASE_PASSWORD"),
-        database=os.getenv("DATABASE_NAME"),
-    )
-    generator = Generator(
-        n_customers=100,
-        n_terminals=50,
-        start_date='2024-12-01',
-        r=5
-    )
-    loader = Loader(db)
-    operations = Operations(db)
+    # load_dotenv()
+    # # clear_dir_path(OUTPUT_DIR)
+    #
+    # db = DatabaseInstance(
+    #     uri=os.getenv("DATABASE_URI"),
+    #     user=os.getenv("DATABASE_USER"),
+    #     password=os.getenv("DATABASE_PASSWORD"),
+    #     database=os.getenv("DATABASE_NAME"),
+    # )
+    # generator = Generator(
+    #     n_customers=100,
+    #     n_terminals=50,
+    #     start_date='2024-12-01',
+    #     r=5
+    # )
+    # loader = Loader(db)
+    # operations = Operations(db)
 
     # generator.generate(
     #     dataset_output_path=DATASET_OUTPUT_DIR,
@@ -37,9 +37,44 @@ def main():
     # loading_time = loader.load_dataset(dataset_path)
     # print(f"Time to load dataset: {loading_time:.3f}s")
 
-    operations.operation_b()
+    # db.close()
 
-    db.close()
+    create_plot(ANALYSIS_OUTPUT_DIR, {
+        "dataset_50MB": {
+            "loading_time": 112.217,
+            "operations": {
+                "a": 182.398,
+                "b": 2.433,
+                "c": 4.406,
+                "d.i": 8.121,
+                "d.ii": 1160.105,
+                "e": 0.607
+            }
+        },
+        "dataset_100MB": {
+            "loading_time": 316.927,
+            "operations": {
+                "a": 520.335,
+                "b": 4.433,
+                "c": 16.392,
+                "d.i": 18.482,
+                "d.ii": 3494.616,
+                "e": 1.090
+            }
+        },
+        "dataset_200MB": {
+            "loading_time": 1082.563,
+            "operations": {
+                "a": 2211.917,
+                "b": 7.204,
+                "c": 57.730,
+                "d.i": 31.476,
+                "d.ii": 7550.757,
+                "e": 1.816
+            }
+        }
+    })
+
 
 
 if __name__ == "__main__":
